8827103fe40d4a9ca9a47294935101b732aea3c9
kundan159 <Kundan135789@gmail.com>
2023-12-03 08:44:13 +0530

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Webpage
+CODSOFT Task2 (Kundan)
+Portfolio Webpage Using HTML,CSS & Js.
---
+++ script.js
@@ -0,0 +1,8 @@
+let about = document.querySelector("a");
+let container = document.querySelector(".home")
+
+
+about.addEventListener("click", function() {
+  about.classList.add("active")
+})
+
